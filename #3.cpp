1a5fea441e54c4891220cc8c3f058fa2dc23a796
Ankit Kumar <167794543+ankitredxm@users.noreply.github.com>
2024-05-01 01:30:03 +0530

    Update #3.cpp

--- #3.cpp
+++ #3.cpp
@@ -1,7 +1,53 @@
+/*
+You are given a number of sticks of varying lengths. You will iteratively cut the sticks into smaller sticks,
+ discarding the shortest pieces until there are none left. 
+At each iteration you will determine the length of the shortest stick remaining, 
+cut that length from each of the longer sticks and then discard all the pieces of that shortest length.
+ When all the remaining sticks are the same length, they cannot be shortened so discard them.
+
+Given the lengths of  sticks, print the number of sticks that are left before each iteration until there are none left.
+The shortest stick length is , so cut that length from the longer two and discard the pieces of length . Now the lengths are . 
+Again, the shortest stick is of length , so cut that amount from the longer stick and discard those pieces.
+ There is only one stick left, , so discard that stick. The number of sticks at each iteration are .*/
+
+
+
+
 #include<iostream>
 using namespace std;
 
-
+void arr(int *p,int size){
+       int count;
+       int shor=0;
+       
+       while(true){
+               
+       for(int i=0;i<size;++i){
+               if(shor>p[i]&&p[i]>0){
+                       shor=p[i];
+               }
+       }
+       int cut=shor;
+               count=0;
+       for(int i=0;i<size;++i){
+               p[i]-=cut;
+               if(p[i]>0){
+                       ++count;
+                       shor=p[i];
+               }
+               
+               
+       }if(count==0){
+               exit(0);
+       }
+       cout<<count<<endl;
+       
+       
+       
+       }
+       
+       
+}
 
 int main(){
      int n;
@@ -11,21 +57,5 @@ int main(){
              cin>>p[i];
      }  
      
-     int shor=p[0];
-        int count;
-       for(int i=1;i<n;++i){
-               if(shor>p[i]){
-                       shor=p[i];
-               }
-       }
-       cout<<n<<endl;
-       while(count!=1){
-               count=0;
-       for(int i=0;i<n;++i){
-               p[i]-=shor;
-               if(p[i]>0){
-                       ++count;
-               }
-       }cout<<count<<endl;
-       }
+     arr(p,n);
 }
